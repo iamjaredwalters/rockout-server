8f40e37d88ec0ae311160ea810512a3e8e7df6d4
Jared Walters <jared.w.walters@gmail.com>
2017-04-30 15:50:37 -0700

    Add mongo

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import 'babel-polyfill';
 import express from 'express';
 import config from './config'; // config handles env configs
+import MongoClient from 'mongodb';
 
 const app = express();
 
-app.set('port', (config.port));
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+MongoClient.connect('mongodb://primary:bleh1845@ds127391.mlab.com:27391/rock', (err, db) => {
+    console.log('Mongo started');
 
-app.listen(app.get('port'), () => {
-    console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+    if (err) throw err;
+
+    app.set('port', (config.port));
+
+    // Express only serves static assets in production
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+    }
+
+    app.listen(app.get('port'), () => {
+        console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+    });
 });
